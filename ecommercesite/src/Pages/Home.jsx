97762df348cec8e267ff7d86155b770e862cdf41
Rushikesh Sapkal <sapkalrushikesh96@gmail.com>
2024-02-11 01:06:58 +0530

    homepage completed

--- ecommercesite/src/Pages/Home.jsx
+++ ecommercesite/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Announce from '../Components/Announce'
 import Navbar from '../Components/Navbar'
+import Slider from '../Components/Slider'
 
 function Home() {
   return (
    <div>
     <Announce/>
     <Navbar/>
+    <Slider/>
    </div>
    
   )
---
+++ ecommercesite/src/api/SliderApi.js
@@ -0,0 +1,23 @@
+export const ApiSlides=[
+    {
+        src:"https://images.unsplash.com/photo-1602810319428-019690571b5b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        content:{
+            h2:"Amazing Deals this Summer",
+            p:"Upto 50% off",
+        }
+    },
+    {
+        src:"https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        content:{
+            h2:"Deal of the Day",
+            p:"Upto 7% off",
+        }
+    },
+    {
+        src:"https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        content:{
+            h2:"Avail awesome Deal this Week",
+            p:"Upto 20% off",
+        }
+    }
+]
